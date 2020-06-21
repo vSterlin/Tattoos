b8bd30620f4aa2db2e7cab4ac8110df2f47c752c
Vladimir Sterlin <vovasterlin@yahoo.com>
2020-06-20 21:39:18 -0700

    test

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,15 +1,15 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+// /**
+//  * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+//  *
+//  * See: https://www.gatsbyjs.org/docs/ssr-apis/
+//  */
 
-// You can delete this file if you're not using it
-import React from "react"
+// // You can delete this file if you're not using it
+// import React from "react"
 
-import { SliderProvider } from "./src/context/SliderContext"
+// import { SliderProvider } from "./src/context/SliderContext"
 
-export const wrapRootElement = ({ element }) => (
-  <SliderProvider>{element}</SliderProvider>
-)
+// export const wrapRootElement = ({ element }) => (
+//   <SliderProvider>{element}</SliderProvider>
+// )
 
